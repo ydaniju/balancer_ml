48a2ae12c66ef4e84f0523d7063d20ab0134ecae
andela-ydaniju <yusuf.daniju@andela.com>
2017-06-01 21:20:46 +0100

    deploy

---
+++ hello.py
@@ -0,0 +1,50 @@
+from numpy import genfromtxt
+import numpy as np
+import statsmodels.api as sm
+
+
+
+def apparent_power(active, reactive):
+    import numpy as np
+    return np.sqrt(np.add(np.square(active), np.square(reactive)))
+
+
+my_data = genfromtxt('turbines.csv', delimiter=',', names=True)
+active = my_data['ACTIVE_POWER']
+reactive = my_data['REACTIVE_POWER']
+
+y = apparent_power(active, reactive)
+
+x = [
+     my_data['WIND_DIRECTION'],
+     my_data['WIND_SPEED'],
+     my_data['HYDRAULIC_OIL_TEMP'],
+     my_data['AMBIENT_TEMP'],
+     my_data['TURBINE_STATUS']
+    ]
+
+def reg_m(y, x):
+    ones = np.ones(len(x[0]))
+    X = sm.add_constant(np.column_stack((x[0], ones)))
+    for ele in x[1:]:
+        X = sm.add_constant(np.column_stack((ele, X)))
+    results = sm.OLS(y, X).fit()
+    return results.params
+
+"""Cloud Foundry test"""
+from flask import Flask
+from flask import jsonify
+
+import os
+
+app = Flask(__name__)
+
+port = int(os.getenv('PORT', '8080'))
+
+@app.route('/')
+def hello_world():
+    result = {'result': reg_m(y, x).tolist()}
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=port)
